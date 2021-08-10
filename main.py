40f7661689c88459e4ea90a4f61d662a18e38888
Avnish327030 <anishyadav7045075175@gmail.com>
2021-08-10 19:53:19 +0530

    ecs task definition added successfully

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    return "Hello avnish!! You did it."
+    return "Hello avnish!! You did it. Let's do the development"
 
 
 if __name__=="__main__":
